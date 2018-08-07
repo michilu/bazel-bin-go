36e03a23981ca2e91a27e83931993ae87b275110
ENDOH takanao <djmchl@gmail.com>
2018-08-07 15:35:34 +0700

    refactor

--- cmd/version.go
+++ cmd/version.go
@@ -1,19 +1,21 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 
 	"github.com/michilu/bazel-bin-go/meta"
 )
 
 func init() {
-	rootCmd.AddCommand(&cobra.Command{
+	rootCmd.AddCommand(newVersion())
+}
+
+func newVersion() *cobra.Command {
+	return &cobra.Command{
 		Use:   "version",
 		Short: "print version",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Print(meta.Get())
+			cmd.Print(meta.Get())
 		},
-	})
+	}
 }
