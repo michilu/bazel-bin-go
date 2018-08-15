b8a2b32ef250643f19f31d94c767059f3fcb89b2
ENDOH takanao <djmchl@gmail.com>
2018-08-15 16:04:17 +0700

    refactor

--- errs/error.go
+++ errs/error.go
@@ -22,22 +22,25 @@ type Error struct {
 // Error returns the string representation of the error message.
 func (e *Error) Error() string {
 	const op = "errs.Error.Error()"
+
+	// `bytes.Buffer.WriteString` always returns a nil error.
+	// https://golang.org/pkg/bytes/#Buffer.WriteString
 	var buf bytes.Buffer
 
 	// Print the current operation in our stack, if any.
 	if e.Op != "" {
-		buf.WriteString(e.Op + ": ") // #nosec err is always nil https://golang.org/pkg/bytes/#Buffer.WriteString
+		buf.WriteString(e.Op + ": ") // #nosec
 	}
 
 	// If wrapping an error, print its Error() message.
 	// Otherwise print the error code & message.
 	if e.Err != nil {
-		buf.WriteString(e.Err.Error()) // #nosec err is always nil https://golang.org/pkg/bytes/#Buffer.WriteString
+		buf.WriteString(e.Err.Error()) // #nosec
 	} else {
 		if e.Code != "" {
-			buf.WriteString("<" + e.Code + "> ") // #nosec err is always nil https://golang.org/pkg/bytes/#Buffer.WriteString
+			buf.WriteString("<" + e.Code + "> ") // #nosec
 		}
-		buf.WriteString(e.Message) // #nosec err is always nil https://golang.org/pkg/bytes/#Buffer.WriteString
+		buf.WriteString(e.Message) // #nosec
 	}
 
 	return buf.String()
