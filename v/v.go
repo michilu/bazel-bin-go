e807b7e6a3a2fabd134439430c20b102184e1d45
ENDOH takanao <djmchl@gmail.com>
2018-08-18 11:03:03 +0700

    fix import

---
+++ v/v.go
@@ -0,0 +1,10 @@
+package v
+
+import (
+	_ "github.com/michilu/bazel-bin-go/v/bus"
+	_ "github.com/michilu/bazel-bin-go/v/cmd"
+	_ "github.com/michilu/bazel-bin-go/v/errs"
+	_ "github.com/michilu/bazel-bin-go/v/log"
+	_ "github.com/michilu/bazel-bin-go/v/meta"
+	_ "github.com/michilu/bazel-bin-go/v/semaphore"
+)
